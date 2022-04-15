4f4e977c37fbd821393ed4259a26eff6bc796568
Carlos Henrique <carloscunha080@gmail.com>
2022-04-15 18:36:06 -0300

    Comecei a estruturar meu programa usando o SOLID.  Criei classe diferentes que possuem apenas um propósito para existir.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,30 @@
 
 namespace CurrencyApp
 {
+    public class CurrencyMenu {
+        public static  void CurrencMenuStart()
+        {
+
+        }
+    }
+
+    class CurrencyApiConection
+    {
+        public string UrlAndress;
+        public CurrencyApiConection(string UrlAndress)
+        {
+            this.UrlAndress = UrlAndress;
+        }
+
+    }
     public class Currency
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HEloo");
+            Console.WriteLine("Bem vindo ao CurrencyApp");
+            
+            
+            
         }
     }
 }
