53aa0a4df5aedd66d1385d9242659ea501dff60c
Carlos Henrique <carloscunha080@gmail.com>
2022-04-17 21:50:59 -0300

    Fiz algumas alerações para melhorar a organização do projeto e crie um projeto novo que serve para testar a aplicação

---
+++ CurrencyAppTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+namespace CurrencyAppTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
